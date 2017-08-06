aea50e46a4e3f89b7a67a02054e3ca07e5927da5
Zorro5 <aaquibmazher54@gmail.com>
2017-08-05 19:06:18 -0700

    Add files via upload

--- ArrayPartition.java
+++ ArrayPartition.java
@@ -1,23 +1,64 @@
-import java.util.Arrays;
-
-public class ArrayPartition {
-
-	 public static int arrayPairSum(int[] nums) {
-		 Arrays.sort(nums);
-		 int sum = 0;
-		 for(int i = nums.length - 1; i > 0; i -= 2){
-			 sum += nums[i - 1];
-			 System.out.println(sum);
-		 }
-		 return sum;
-	    }
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+ 
+ 
+ 
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        // Two pointer approach!
 		
-		int array[] = {0,-1,-2,-3};
-		System.out.println("Max Sum: " + arrayPairSum(array));
-
-	}
-
+        Arrays.sort(nums1); 
+        Arrays.sort(nums2);
+        List<Integer> list = new ArrayList();
+        int index1 = 0, index2 = 0;
+		
+		//Loop until the end of the shorter array
+        while(index1 < nums1.length && index2 < nums2.length){
+			
+			//Keep moving the pointers accordingly, as the arrays are sorted now. 
+            if(nums1[index1] < nums2[index2]){
+                index1++;
+            }
+            else if(nums1[index1] > nums2[index2]){
+                index2++;
+            }
+            else{
+                list.add(nums1[index1]);
+                index1++;
+                index2++;
+            }
+        }
+        
+        int result[] = new int[list.size()];
+        for(int i = 0; i < result.length; i++){
+			result[i] = list.get(i); 
+		}
+		return result;
+        
+        /*
+    	//int result[] = new int[20];
+		List<Integer> list = new ArrayList();
+		Map<Integer, Integer> map = new HashMap();
+		for(int i : nums1){
+			if(map.containsKey(i)){
+				map.put(i, map.get(i) + 1);
+			}
+			else map.put(i, 1);
+		}
+		
+		int index = 0;
+		for(int i : nums2){
+			if(map.containsKey(i) && map.get(i) > 0){
+				map.put(i, map.get(i) - 1);
+                list.add(i);
+			}
+			//result[index++] = i;
+		}
+		
+		int result[] = new int[list.size()];
+		for(int i = 0; i < result.length; i++){
+			result[i] = list.get(i); 
+		}
+		return result;
+        */
+        
+    }
 }
