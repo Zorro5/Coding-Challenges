df72014be4844876a4ef3665b848d796d4173740
Aaquib <aaquibmazher54@gmail.com>
2020-12-01 18:24:26 +0530

    Update impl

--- AddBinaryStrings.java
+++ AddBinaryStrings.java
@@ -3,7 +3,12 @@ public class AddBinaryStrings {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String str1 = "1010010";
+		String str2 = "0101001";
+		System.out.println("Sum of the two Binary Strings is: " + addBinaryStrings(str1, str2));
 	}
-
+	
+	private String addBinaryStrings(String str1, String str2) {
+		//TODO Implementation
+	}
 }
